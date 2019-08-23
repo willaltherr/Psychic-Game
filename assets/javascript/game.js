9c887adc4a5df1b292dd401863dc6646e836f183
Wills Grave <will@keydigitalconsulting.com>
2019-08-23 18:56:04 -0400

    Added HTML for game and pseudocoded the game.

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+// Pseudocode the steps
+
+// Create an array of the letters
+
+
+
+// Create Global Variables
+
+
+
+
+// Pick Random Choices
+
+
+
+// Track Guesses Reamaining
+
+
+
+// Track User Picks
+
+
+
+// Tracks Wins & Losses
