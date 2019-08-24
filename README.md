0b9f87ad1325a825c8283a8b87b8311345fb6997
Wills Grave <will@keydigitalconsulting.com>
2019-08-24 13:15:15 -0400

    Added the guesses remaining, user picks, and wins/losses.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Psychic-Game
 Basic HW #3 for Javascript
+
+1. What the project does:
+This project will allow a user to try to guess the correct letter that is being choosen randomly in the javascript.  They only receive 9 guesses before they lose and the game starts over.
+
+2. Why the project is useful:
+This project is useful becuase it teaches us javascript which will allow us to make the website interactive and updates accordingly.  This is useful for many businesses since most websites require some sort of javascript.
+
+3. How users can get started with the project:
+It is important to start with pseudocode and break down the project into smaller sections.  Once all sections have been identifited then you can begin each section one at a time.
+
+4. Where users can get help with your project:
+Everything is posted in the repo and that is where you can get help understanding how to accomplish the html, css and javascript of this assignment/game.
+
+5. Who maintains and contributes to the project:
+This project was maintained and contributed by Will Altherr.  In order for me to complete this project I had to use resources within the following: https://www.w3schools.com/js/, https://www.youtube.com/watch?v=BxFi7vVZx4s, https://www.youtube.com/watch?v=f5BbzXgvi1o&t=4815s, https://github.com/Kbowen200247/Psychic-Game, https://help.github.com/en/articles/about-readmes
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,19 +9,53 @@ var wins = 0;
 var losses = 0;
 var remainingGuesses = 9;
 var playerPick = [];
+var letters = null;
 
 
 // Pick Random Choices
 
-var playerGuess = guessOptions[Math.floor(Math.random() * guessOptions.length)];
-// console.log(playerGuess);
+var randGuess = guessOptions[Math.floor(Math.random() * guessOptions.length)];
 
-// Track Guesses Reamaining
+// Track Guesses Remaining
+function countRemainingGuesses() {
+  document.querySelector("#remainingGuesses").innerHTML = "# of Guesses Remaining: " + remainingGuesses;
+}
 
+// Add to empty array for player picks
+function farUserGuesses() {
+  document.querySelector("#guessesPicked").innerHTML = "The Guesses You've Already Picked: " + playerPick.join(' ');
+}
 
+countRemainingGuesses();
+
+var restart = function() {
+  remainingGuesses = 9;
+  playerPick = [];
+  var randGuess = guessOptions[Math.floor(Math.random() * guessOptions.length)];
+}
 
 // Track User Picks
 
+document.onkeyup = function(event) {
+  remainingGuesses--;
+
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+  playerPick.push(userGuess);
+  countRemainingGuesses();
+  farUserGuesses();
+
+  // Tracks Wins & Losses
+  if (userGuess === randGuess){
+    wins++;
+    document.querySelector("#wins").innerHTML = "# of Wins: " + wins;
+    restart();
+  } 
+  else if (remainingGuesses === 0) {
+    losses++;
+    document.querySelector("#losses").innerHTML = "# of Losses: " + losses;
+    restart();
+  }
+  };
 
 
-// Tracks Wins & Losses
