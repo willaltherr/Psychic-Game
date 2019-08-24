e49a77abbd8664ca6e0b801bf4131e6839a788dd
Wills Grave <will@keydigitalconsulting.com>
2019-08-24 07:54:58 -0400

    Added alphabet array and random guess.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,15 +2,15 @@
 
 // Create an array of the letters
 
-
+var guessOptions = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 // Create Global Variables
 
 
 
-
 // Pick Random Choices
 
+var playerGuess = guessOptions[Math.floor(Math.random() * playerGuess.length)];
 
 
 // Track Guesses Reamaining
